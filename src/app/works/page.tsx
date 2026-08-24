ae9aaedc65c366451030b3c6e5dee4c6654bf0b2
sardarit-bd <236187923+sardarit-bd@users.noreply.github.com>
2026-08-24 12:50:15 +0600

    new pages added and update

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import Hero from "./components/hero";
-import Projects from "../../components/Projects";
 import Cta from "@/components/Cta";
+import Projects from "../../components/sections/projects";
 export default function page() {
   return (
-    <div className="min-h-screen">
-      <Hero />
+    <div className="w-full bg-white text-neutral-900 overflow-x-clip pt-16 md:pt-20">
+      {/* <Hero /> */}
       <Projects />
       <Cta />
     </div>
--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,15 +1,21 @@
 "use client";
-import ProjectCardVartical from "./ui/ProjectCardVertical";
-import SectionHeader from "./ui/SectionHeader";
+import ProjectCardVartical from "../ui/ProjectCardVertical";
+import SectionHeader from "../ui/SectionHeader";
 export default function Projects() {
   return (
-    <section className="py-16 md:py-20 flex flex-col gap-10 bg-gray-200">
+    <section className="py-16 md:py-14 flex flex-col gap-10 bg-gray-200">
       <div className="container mx-auto px-6 md:px-12 flex flex-col md:flex-row md:items-end md:justify-between gap-8">
-
-        <SectionHeader tag={"Selected work"} title1="Products we've" pre="" title2="designed,built, and shipped." isBgWhite={true} link={'/'} btn={'See all case studies'} />
+        <SectionHeader
+          tag={"Selected work"}
+          title1="Products we've"
+          pre=""
+          title2="designed,built, and shipped."
+          isBgWhite={true}
+          link={""}
+          btn={""}
+        />
       </div>
       <div className="container mx-auto px-6 md:px-12 flex flex-col md:items-end md:justify-between gap-8">
-        {/* Featured Project */}
         <div className="w-full flex flex-col lg:flex-row gap-5">
           <ProjectCardVartical
             eyebrow="Real Estate Website"
@@ -18,7 +24,7 @@ export default function Projects() {
             statValue="4"
             statLabel="services delivered"
             ctaLabel="View Details"
-            ctaHref="/projects/casa-viva"
+            ctaHref="/works/casa-viva"
             imageSrc="https://cdn.prod.website-files.com/697722e913f661fc1b49692f/697722e913f661fc1b4970fb_Rivertel-UI-UX-Design-for-Telecom-Solutions.webp"
             imageAlt="CASA VIVA Real Estate Website"
           />
@@ -30,13 +36,12 @@ export default function Projects() {
             statValue="3"
             statLabel="services delivered"
             ctaLabel="View Details"
-            ctaHref="/projects/medease"
+            ctaHref="/works/medease"
             imageSrc="https://cdn.prod.website-files.com/697722e913f661fc1b49692f/697722e913f661fc1b4970b7_Abyan-Capital-Trading-Mobile-App.webp"
             imageAlt="MedEase Hospital Management"
           />
         </div>
 
-        {/* Two Projects */}
         <div className="w-full flex flex-col lg:flex-row gap-5">
           <ProjectCardVartical
             eyebrow="Medical Website"
@@ -45,7 +50,7 @@ export default function Projects() {
             statValue="3"
             statLabel="services delivered"
             ctaLabel="View Details"
-            ctaHref="/project/White_Cross_Clinic.webp"
+            ctaHref="/works/White_Cross_Clinic.webp"
             imageSrc="https://cdn.prod.website-files.com/697722e913f661fc1b49692f/697722e913f661fc1b4970b7_Abyan-Capital-Trading-Mobile-App.webp"
             imageAlt="White Cross Clinic Website"
           />
@@ -57,16 +62,12 @@ export default function Projects() {
             statValue="3"
             statLabel="services delivered"
             ctaLabel="View Details"
-            ctaHref="/projects/gulf-county"
+            ctaHref="/works/gulf-county"
             imageSrc="https://cdn.prod.website-files.com/697722e913f661fc1b49692f/697722e913f661fc1b4970b7_Abyan-Capital-Trading-Mobile-App.webp"
             imageAlt="Gulf County Music Application"
           />
         </div>
 
-
-
-
-        {/* Last Project */}
         <div className="w-full flex flex-col lg:flex-row gap-5">
           <ProjectCardVartical
             eyebrow="Home Services Website"
@@ -75,12 +76,11 @@ export default function Projects() {
             statValue="3"
             statLabel="services delivered"
             ctaLabel="View Details"
-            ctaHref="/projects/home-service-provider"
+            ctaHref="/works/home-service-provider"
             imageSrc="https://cdn.prod.website-files.com/697722e913f661fc1b49692f/697722e913f661fc1b4970b7_Abyan-Capital-Trading-Mobile-App.webp"
             imageAlt="Home Service Provider Website"
           />
 
-          {/* Optional placeholder for future project */}
           <ProjectCardVartical
             eyebrow="Coming Soon"
             title="Next Project"
@@ -88,7 +88,7 @@ export default function Projects() {
             statValue="Soon"
             statLabel="launching"
             ctaLabel="Coming Soon"
-            ctaHref="#"
+            ctaHref="/works/comeingsoon"
             imageSrc="https://cdn.prod.website-files.com/697722e913f661fc1b49692f/697722e913f661fc1b4970b7_Abyan-Capital-Trading-Mobile-App.webp"
             imageAlt="Upcoming Project"
           />
